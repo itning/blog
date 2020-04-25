cd6af0d6f8b3f9f1d34bcd1c4a131d0ef4d23f0f
itning <itning@itning.top>
2020-04-25 11:31:30 +0800

    build: add docs

--- push.bat
+++ push.bat
@@ -1,7 +1,5 @@
 @echo off
 SET NOW_TIME=%date:~0,4%%date:~5,2%%date:~8,2%0%time:~1,1%%time:~3,2%%time:~6,2%
-hugo --minify
-git add .\docs
 git add .\content\post\*
 git add .\static\images\*
 git commit -m "NEW-POST-%NOW_TIME%"
