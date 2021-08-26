c4cd78f74e9e1ad1361f321cdeaa68e0ed412664
itning <itning@itning.top>
2021-08-26 17:47:09 +0800

    feat: cos

--- push.bat
+++ push.bat
@@ -3,4 +3,6 @@ SET NOW_TIME=%date:~0,4%%date:~5,2%%date:~8,2%0%time:~1,1%%time:~3,2%%time:~6,2%
 git add .\content\post\*
 git add .\static\images\*
 git commit -m "NEW-POST-%NOW_TIME%"
+git config --global https.proxy http://127.0.0.1:7890
 git push origin master
+git config --global --unset http.proxy
